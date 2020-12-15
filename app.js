de7a9fea66bca3a37e25523ce1de9d29a6595b0e
Tom Corley <thomcorley@gmail.com>
2020-12-15 17:01:58 +0000

    WIP: get images to display on homepage
    * tidied up indentation
    
    TODO:
    * figure out why the images are not showing

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import recipes from "./recipes.js"
 
 function displayThumbnail(recipe) {
-  let section = document.querySelector("section.posts.clear")
-  let article = document.createElement("article");
+  const section = document.querySelector("section.posts.clear")
+  const article = document.createElement("article");
 
   article.className = "post"
 
   article.innerHTML = `
-    <h2 class="post-title" itemprop="name headline">${recipe.name}</h2>
+    <h2 class="post-title">${recipe.name}</h2>
 
     <div class="post-content">
       <p><img src="${recipe.image}" alt="${recipe.name}" /></p>
